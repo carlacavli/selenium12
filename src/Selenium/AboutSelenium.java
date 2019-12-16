4e0a70454a01834357140d19cf063dee01c3c09b
muberracavli <muberracav@hotmail.com>
2019-12-16 13:19:22 -0500

    Exercises,notes

---
+++ src/Selenium/AboutSelenium.java
@@ -0,0 +1,45 @@
+package Selenium;
+
+public class AboutSelenium {
+    /*
+    What is Selenium?
+   Selenium is a free (open source) automated testing suite for web applications across different browsers and platforms.
+
+   Advantages of Selenium
+
+   The following list showcases the advantages of Selenium testing tool that makes it best among other automation testing tools.
+Selenium is an open-source automation testing tool and it is free of cost to use.
+Selenium provides high tester flexibility to write advanced and complex test cases.
+Supports test scripts written in any user-preferred languages such as C#, Java, Perl, PHP, Python, and Ruby
+Supports test case execution on multiple operating systems such as Windows, Linux, Android, Mac, and iOS.
+Supports testing on different web browsers such as Chrome, Firefox, Internet Explorer (IE), Opera, and Safari.
+Test cases can be executed while the browser window is minimized.
+Selenium supports parallel test execution.
+Selenium can be integrated with TestNG and JUnit to generate test reports and manage test cases.
+Selenium can be integrated with Jenkins, Docker, and Maven to attain continuous testing.
+
+  Apart from the above advantages, Selenium testing tool has some shortcomings such as,
+
+  Selenium can test only web applications. It cannot test software applications and other desktop applications.
+Selenium cannot access the web elements outside the web applications that are under test.
+No guaranteed user support is available. We have to depend on customer communities.
+Selenium independently does not support image testing. It must be integrated with Sikuli for image testing.
+
+Conclusion
+Selenium testing tool has both advantages and shortcomings in comparison to other automation testing tools.
+ But, Selenium overshadows other testing tools in areas of –
+Cost – Selenium comes at free of cost as it is an open-source testing tool.
+Parallel Testing – Tester can execute test scripts on multiple machines simultaneously.
+Flexibility – Selenium supports test scripts in multiple languages, execution on multiple browsers and operating systems.
+On the conclusion note, the above three major features of Selenium and its different components made Selenium as one of the best automation testing tool.
+
+
+
+
+
+
+
+
+     */
+
+}
