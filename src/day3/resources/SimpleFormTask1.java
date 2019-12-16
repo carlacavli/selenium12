78d66d12fc3829329d4d4561aa99715ed77adc90
muberracavli <muberracav@hotmail.com>
2019-12-15 20:05:14 -0500

    day3

---
+++ src/day3/resources/SimpleFormTask1.java
@@ -0,0 +1,41 @@
+package day3.resources;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class SimpleFormTask1 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/muberracavli/Desktop/TechnoStudy/chrome driver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+        WebElement message = driver.findElement( By.id( "user-message" ) );
+        String myMessage = "Test string!";
+        message.sendKeys( myMessage );
+
+        List<WebElement> buttons = driver.findElements( By.tagName( "button" ) );
+        for(WebElement button: buttons){
+//            System.out.println(button.getText());
+            if(button.getText().equals( "Show Message" )){
+                button.click();
+            }
+        }
+
+        WebElement display = driver.findElement( By.id( "display" ) );
+        String text = display.getText();
+
+        if(text.equals( myMessage )){
+            System.out.println("Success!");
+        } else {
+            System.out.println("Failure!");
+        }
+
+
+        driver.quit();
+    }
+}
+
+
---
+++ src/day3/resources/SimpleFormTask2.java
@@ -0,0 +1,54 @@
+package day3.resources;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+// https://www.seleniumeasy.com/test/basic-first-form-demo.html
+// Fill in Two Input Fields with numbers, press the button and check result
+
+public class SimpleFormTask2 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/muberracavli/Desktop/TechnoStudy/chrome driver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+
+        Integer number1 = 100;
+        Integer number2 = 222;
+
+        WebElement sum1 = driver.findElement( By.id( "sum1" ) );
+        sum1.sendKeys( number1.toString() );
+
+        WebElement sum2 = driver.findElement( By.id( "sum2" ) );
+        sum2.sendKeys( number2.toString() );
+
+        List<WebElement> buttons = driver.findElements( By.tagName( "button" ) );
+        for(WebElement button : buttons) {
+//            System.out.println(button.getText());
+            if(button.getText().equals( "Get Total" )) {
+                button.click();
+            }
+        }
+
+        WebElement display = driver.findElement( By.id( "displayvalue" ) );
+        String text = display.getText();
+
+        if(text.equals( String.valueOf( number1 + number2 ) )) {
+            System.out.println( "Success!" );
+        } else {
+            System.out.println( "Failure!" );
+        }
+
+        driver.quit();
+    }
+}
+
+
+
+
+
+
+    
+
