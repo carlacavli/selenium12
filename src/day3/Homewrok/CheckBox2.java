5cda1e2e1d53a8c0de549538725403ae19712f10
muberracavli <muberracav@hotmail.com>
2019-12-18 14:10:50 -0500

    homework

--- src/day3/Homewrok/CheckBox2.java
+++ src/day3/Homewrok/CheckBox2.java
@@ -1,13 +1,36 @@
 package day3.Homewrok;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class CheckBox2 {
     // https://www.seleniumeasy.com/test/basic-checkbox-demo.html
     // Check the Multiple Checkbox and check the button message
     // When you check all the checkboxes, button message will change to 'Uncheck All'
     // When you uncheck at least one checkbox, button message will change to 'Check All'
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/muberracavli/Desktop/TechnoStudy/chrome driver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
 
+        List<WebElement> checkboxes = driver.findElements(By.className("cb1-element"));
+        for (WebElement check : checkboxes) {
+            check.click();
+        }
 
-    }
+            WebElement message = driver.findElement(By.id("check1"));
+            String myMessage = " Uncheck All";
 
-}
+        if(message.getText().equals( myMessage )){
+            System.out.println("Success!");
+        } else {
+            System.out.println("Failure!");
+        }
+            }
+
+        }
+
