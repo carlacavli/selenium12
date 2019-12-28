881d9c8e14a839937535a3fc2f8b5cab81e9938c
muberracavli <muberracav@hotmail.com>
2019-12-27 19:03:43 -0500

    Day3homework

--- src/day3/Homewrok/CheckBox2.java
+++ src/day3/Homewrok/CheckBox2.java
@@ -12,7 +12,7 @@ public class CheckBox2 {
     // Check the Multiple Checkbox and check the button message
     // When you check all the checkboxes, button message will change to 'Uncheck All'
     // When you uncheck at least one checkbox, button message will change to 'Check All'
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/Users/muberracavli/Desktop/TechnoStudy/chrome driver/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
@@ -22,15 +22,25 @@ public class CheckBox2 {
             check.click();
         }
 
-            WebElement message = driver.findElement(By.id("check1"));
-            String myMessage = " Uncheck All";
+//        WebElement message = driver.findElement(By.id("check1"));
+//        WebElement message2 =driver.findElement(By.className("cb1-element"));
+//        String myMessage = " Uncheck All";
+//        String myMessage1 = "Check All";
+//        if(message.getText().equals( myMessage )){
+//            System.out.println("success!");
+//        }else{
+//            System.out.println("fail");
+//        }
+//        if(!message2.getText().equals(myMessage1)){
+//            System.out.println("success!");
+//        }else{
+//            System.out.println("fail");
+//        }
 
-        if(message.getText().equals( myMessage )){
-            System.out.println("Success!");
-        } else {
-            System.out.println("Failure!");
-        }
-            }
-
-        }
-
+        driver.findElement(By.id("check1")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@value='Uncheck All']")).click();
+        Thread.sleep(2000);
+        driver.quit();
+    }
+    }
